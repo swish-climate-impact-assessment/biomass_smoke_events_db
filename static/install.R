3ee5112fbe998419457cb7c5dc4393e14d568db8
ivanhanigan <ivan.hanigan@gmail.com>
2016-03-19 14:13:09 +1100

    installer works better

--- static/install.R
+++ static/install.R
@@ -4,6 +4,7 @@
 if on linux_os I assume you will use this from user_home (~/)
 else assume windows and I assume you will use web2py from C:/Temp
 "
+download_web2py <- FALSE
 # function to decide which download of web2py, linux or win
 linux_os <- function(){
   if(length(grep('linux',sessionInfo()[[1]]$os)) == 1)
@@ -23,20 +24,32 @@ linux_os <- function(){
     os <- 'linux?'
     OsLinux <- TRUE
   }
+}
 if(linux_os()){
+if(!file.exists("~/web2py_src.zip")) download_web2py <- TRUE
+  if(download_web2py){
   # I assume you will use this from ~/
   download.file("http://web2py.com/examples/static/web2py_src.zip",
                 destfile = "~/web2py_src.zip", mode = "wb")
   unzip("~/web2py_src.zip")
+  }
   setwd("~/web2py/applications/")
 } else {
+if(!file.exists("C:/Temp/web2py_win.zip")) download_web2py <- TRUE
+  if(download_web2py){
   # I assume you will use web2py from the top of C
   dir.create("C:/Temp",showWarnings = F)
   download.file("http://web2py.com/examples/static/web2py_win.zip",
                 destfile = "C:/Temp/web2py_win.zip", mode = "wb")
   setwd("C:/Temp/")
   unzip("web2py_win.zip")
-  setwd("web2py/applications/")
+  }
+  setwd("C:/Temp/web2py/applications/")
+}
+if(file.exists("biomass_smoke_events_db")){
+   file.rename("biomass_smoke_events_db",
+               sprintf("biomass_smoke_events_db_%s",make.names(Sys.time()))
+               )    
 }
 
 if(!require(downloader)) install.packages("downloader");
@@ -48,14 +61,22 @@ file.rename("biomass_smoke_events_db-master", "biomass_smoke_events_db")
 
 if(linux_os()){
   setwd("~/web2py")
+  sink("w2p.py")
+cat("python web2py.py -a xpassword -i 0.0.0.0 -p 8181")
+cat("firefox http://127.0.0.1:8181/biomass_smoke_events_db")  
+  sink()
+  print(cat('you have a go script in ~/web2py/w2p.py\n
+bash it to run it\n'))
+# or just try to do it from R
   system("python web2py.py -a xpassword -i 0.0.0.0 -p 8181", wait = F)
   browseURL("http://127.0.0.1:8181/biomass_smoke_events_db")
 
 } else {
   setwd("C:/Temp/")
   sink("w2p.cmd")
-cat("cd C:/Temp/web2py\nweb2py.exe -a xpassword -i 0.0.0.0 -p 8181")
-cat("cmd /c start filename_or_URL")  
+cat("cd C:/Temp/web2py\n")
+cat("start web2py.exe -a xpassword -i 0.0.0.0 -p 8181\n")
+cat("cmd /c start http://127.0.0.1:8181/biomass_smoke_events_db")  
   sink()
   print(cat('you have a batch file now: C:/Temp/w2p.cmd\n
 Double click to run it\n'))
